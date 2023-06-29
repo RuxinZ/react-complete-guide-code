5926702fa472e462f580677cdb4f38fba469299d
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-06-29 10:56:22 -0700

    complete update

--- code/react-vs-vanilla-js-example/react/src/App.js
+++ code/react-vs-vanilla-js-example/react/src/App.js
@@ -1,24 +1,30 @@
-import { useState } from "react";
-import "./styles.css";
+import { useState } from 'react';
+import './styles.css';
 
 const content = [
   [
-    "React is extremely popular",
-    "It makes building complex, interactive UIs a breeze",
+    'React is extremely popular',
+    'It makes building complex, interactive UIs a breeze',
     "It's powerful & flexible",
-    "It has a very active and versatile ecosystem"
+    'It has a very active and versatile ecosystem',
   ],
   [
-    "Components, JSX & Props",
-    "State",
-    "Hooks (e.g., useEffect())",
-    "Dynamic rendering"
+    'Components, JSX & Props',
+    'State',
+    'Hooks (e.g., useEffect())',
+    'Dynamic rendering',
   ],
   [
-    "Official web page (react.dev)",
-    "Next.js (Fullstack framework)",
-    "React Native (build native mobile apps with React)"
-  ]
+    'Official web page (react.dev)',
+    'Next.js (Fullstack framework)',
+    'React Native (build native mobile apps with React)',
+  ],
+  [
+    'This is another sentence',
+    'haha another one',
+    'a third one',
+    'I have more',
+  ],
 ];
 
 export default function App() {
@@ -37,27 +43,33 @@ export default function App() {
       <div id="tabs">
         <menu>
           <button
-            className={activeContentIndex === 0 ? "active" : ""}
+            className={activeContentIndex === 0 ? 'active' : ''}
             onClick={() => setActiveContentIndex(0)}
           >
             Why React?
           </button>
           <button
-            className={activeContentIndex === 1 ? "active" : ""}
+            className={activeContentIndex === 1 ? 'active' : ''}
             onClick={() => setActiveContentIndex(1)}
           >
             Core Features
           </button>
           <button
-            className={activeContentIndex === 2 ? "active" : ""}
+            className={activeContentIndex === 2 ? 'active' : ''}
             onClick={() => setActiveContentIndex(2)}
           >
             Related Resources
           </button>
+          <button
+            className={activeContentIndex === 3 ? 'active' : ''}
+            onClick={() => setActiveContentIndex(3)}
+          >
+            Added Tab
+          </button>
         </menu>
         <div id="tab-content">
           <ul>
-            {content[activeContentIndex].map((item) => (
+            {content[activeContentIndex].map(item => (
               <li key={item}>{item}</li>
             ))}
           </ul>
