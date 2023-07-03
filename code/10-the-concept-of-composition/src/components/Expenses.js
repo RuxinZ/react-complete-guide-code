e0e20c76e0ce7d5d5965966c5912e84dbeb87180
Ruxin Zheng <RuxinZ@users.noreply.github.com>
2023-07-03 15:10:08 -0700

    complete composition practice

--- code/10-the-concept-of-composition/src/components/Expenses.js
+++ code/10-the-concept-of-composition/src/components/Expenses.js
@@ -3,29 +3,18 @@ import Card from './Card';
 import './Expenses.css';
 
 function Expenses(props) {
+  const items = props.items.map((obj, i) => (
+    <ExpenseItem
+      key={i}
+      id={obj.id}
+      title={obj.title}
+      amount={obj.amount}
+      date={obj.date}
+    />
+  ));
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
-    </Card>
+    // Card is a wrapper
+    <Card className="expenses">{items}</Card>
   );
 }
 
